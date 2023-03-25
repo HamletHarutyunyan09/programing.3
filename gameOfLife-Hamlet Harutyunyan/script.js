217b4d7b22596116f2ca139ac1dba488903b6017
HamletHarutyunyan <hamlet.h8@tumo.org>
2023-03-25 16:18:26 +0400

    serverjs

--- gameOfLife-Hamlet Harutyunyan/script.js
+++ gameOfLife-Hamlet Harutyunyan/script.js
@@ -1,86 +1,86 @@
-function matrixGenerator(matrixSize, grass, grassEater, predator, mrjyun, agrav, mexu) {
-    var matrix = []
+// function matrixGenerator(matrixSize, grass, grassEater, predator, mrjyun, agrav, mexu) {
+//     var matrix = []
 
-    for (let i = 0; i < matrixSize; i++) {
-        matrix.push([])
-        for (let j = 0; j < matrixSize; j++) {
-            matrix[i].push(0)
+//     for (let i = 0; i < matrixSize; i++) {
+//         matrix.push([])
+//         for (let j = 0; j < matrixSize; j++) {
+//             matrix[i].push(0)
 
-        }
-    }
-
-
-    for (let i = 0; i < grass; i++) {
-
-        var x = Math.floor(Math.random() * matrixSize)
-        var y = Math.floor(Math.random() * matrixSize)
-
-        matrix[y][x] = 1
-
-    }
-
-    for (let i = 0; i < grassEater; i++) {
-
-        var x = Math.floor(Math.random() * matrixSize)
-        var y = Math.floor(Math.random() * matrixSize)
-
-        matrix[y][x] = 2
-
-    }
+//         }
+//     }
 
 
+//     for (let i = 0; i < grass; i++) {
 
-    for (let i = 0; i < predator; i++) {
+//         var x = Math.floor(Math.random() * matrixSize)
+//         var y = Math.floor(Math.random() * matrixSize)
 
-        var x = Math.floor(Math.random() * matrixSize)
-        var y = Math.floor(Math.random() * matrixSize)
+//         matrix[y][x] = 1
 
-        matrix[y][x] = 3
+//     }
 
+//     for (let i = 0; i < grassEater; i++) {
 
-    }
+//         var x = Math.floor(Math.random() * matrixSize)
+//         var y = Math.floor(Math.random() * matrixSize)
 
-    for (let i = 0; i < mrjyun; i++) {
+//         matrix[y][x] = 2
 
-        var x = Math.floor(Math.random() * matrixSize)
-        var y = Math.floor(Math.random() * matrixSize)
-
-        matrix[y][x] = 4
-    }
-
-    for (let i = 0; i < agrav; i++) {
-
-        var x = Math.floor(Math.random() * matrixSize)
-        var y = Math.floor(Math.random() * matrixSize)
-
-        matrix[y][x] = 5
-
-    }
-    for (let i = 0; i < mexu; i++) {
-
-        var x = Math.floor(Math.random() * matrixSize)
-        var y = Math.floor(Math.random() * matrixSize)
-
-        matrix[y][x] = 6
-
-    return matrix
-
-    }
+//     }
 
 
 
-}
+//     for (let i = 0; i < predator; i++) {
+
+//         var x = Math.floor(Math.random() * matrixSize)
+//         var y = Math.floor(Math.random() * matrixSize)
+
+//         matrix[y][x] = 3
+
+
+//     }
+
+//     for (let i = 0; i < mrjyun; i++) {
+
+//         var x = Math.floor(Math.random() * matrixSize)
+//         var y = Math.floor(Math.random() * matrixSize)
+
+//         matrix[y][x] = 4
+//     }
+
+//     for (let i = 0; i < agrav; i++) {
+
+//         var x = Math.floor(Math.random() * matrixSize)
+//         var y = Math.floor(Math.random() * matrixSize)
+
+//         matrix[y][x] = 5
+
+//     }
+//     for (let i = 0; i < mexu; i++) {
+
+//         var x = Math.floor(Math.random() * matrixSize)
+//         var y = Math.floor(Math.random() * matrixSize)
+
+//         matrix[y][x] = 6
+
+//     return matrix
+
+//     }
+
+
+
+//}
 /////
-var matrix = matrixGenerator(30, 40, 15, 5, 6,8,10)
+// var matrix = matrixGenerator(30, 40, 15, 5, 6,8,10)
 var side = 25
 //
 
-var grassArr = []
-var grassEaterArr = []
-var predatorArr = []
-var mrjyunArr = []
-var agravArr = []
-var mexuArr = []
+// var grassArr = []
+// var grassEaterArr = []
+// var predatorArr = []
+// var mrjyunArr = []
+// var agravArr = []
+// var mexuArr = []
 
 ////////
 function setup() {
@@ -117,37 +117,37 @@ function setup() {
 ////
 function draw() {
 
-    for (let y = 0; y < matrix.length; y++) {
-        for (let x = 0; x < matrix[y].length; x++) {
+    // for (let y = 0; y < matrix.length; y++) {
+    //     for (let x = 0; x < matrix[y].length; x++) {
 
-            if (matrix[y][x] == 1) {
-                fill("green")
-            } else if (matrix[y][x] == 2) {
-                fill("yellow")
-            } else if (matrix[y][x] == 3) {
-                fill("red")
-            } else if (matrix[y][x] == 4) {
-                fill("brown")
-            } else if (matrix[y][x] == 5) {
-                fill("black")
-            } else if (matrix[y][x] == 6) {
-                fill("orange")
-            }else {
-                fill("gray")
-            }
-            rect(x * side, y * side, side, side)
-        }
+    //         if (matrix[y][x] == 1) {
+    //             fill("green")
+    //         } else if (matrix[y][x] == 2) {
+    //             fill("yellow")
+    //         } else if (matrix[y][x] == 3) {
+    //             fill("red")
+    //         } else if (matrix[y][x] == 4) {
+    //             fill("brown")
+    //         } else if (matrix[y][x] == 5) {
+    //             fill("black")
+    //         } else if (matrix[y][x] == 6) {
+    //             fill("orange")
+    //         }else {
+    //             fill("gray")
+    //         }
+    //         rect(x * side, y * side, side, side)
+    //     }
 
-    }
+    // }
 
-    for (let i in grassArr) {
-        grassArr[i].mul()
-    }
+    // for (let i in grassArr) {
+    //     grassArr[i].mul()
+    // }
 
-    for (let i in grassEaterArr) {
-        grassEaterArr[i].eat()
+    // for (let i in grassEaterArr) {
+    //     grassEaterArr[i].eat()
 
-    }
+    // }
 
 
 
---
+++ gameOfLife-Hamlet Harutyunyan/server.js
@@ -0,0 +1,162 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+var messages = [];
+app.use(express.static("."));
+app.get('/', function (req, res) {
+    res.redirect('index.html');
+});
+
+server.listen(3000, function () {
+    console.log("server okay")
+});
+
+io.on('connection', function (socket) {
+    for (var i in messages) {
+        socket.emit("display message", messages[i]);
+    }
+    socket.on("send message", function (data) {
+        messages.push(data);
+        io.sockets.emit("display message", data);
+    });
+});
+
+function matrixGenerator(matrixSize, grass, grassEater, predator, mrjyun, agrav, mexu) {
+    var matrix = []
+
+    for (let i = 0; i < matrixSize; i++) {
+        matrix.push([])
+        for (let j = 0; j < matrixSize; j++) {
+            matrix[i].push(0)
+
+        }
+    }
+
+
+    for (let i = 0; i < grass; i++) {
+
+        var x = Math.floor(Math.random() * matrixSize)
+        var y = Math.floor(Math.random() * matrixSize)
+
+        matrix[y][x] = 1
+
+    }
+
+    for (let i = 0; i < grassEater; i++) {
+
+        var x = Math.floor(Math.random() * matrixSize)
+        var y = Math.floor(Math.random() * matrixSize)
+
+        matrix[y][x] = 2
+
+    }
+
+
+
+    for (let i = 0; i < predator; i++) {
+
+        var x = Math.floor(Math.random() * matrixSize)
+        var y = Math.floor(Math.random() * matrixSize)
+
+        matrix[y][x] = 3
+
+
+    }
+
+    for (let i = 0; i < mrjyun; i++) {
+
+        var x = Math.floor(Math.random() * matrixSize)
+        var y = Math.floor(Math.random() * matrixSize)
+
+        matrix[y][x] = 4
+    }
+
+    for (let i = 0; i < agrav; i++) {
+
+        var x = Math.floor(Math.random() * matrixSize)
+        var y = Math.floor(Math.random() * matrixSize)
+
+        matrix[y][x] = 5
+
+    }
+    for (let i = 0; i < mexu; i++) {
+
+        var x = Math.floor(Math.random() * matrixSize)
+        var y = Math.floor(Math.random() * matrixSize)
+
+        matrix[y][x] = 6
+
+        return matrix
+
+    }
+
+
+
+}
+
+matrix = matrixGenerator(30, 40, 15, 5, 6, 8, 10)
+grassArr = []
+grassEaterArr = []
+predatorArr = []
+mrjyunArr = []
+mexuArr = []
+
+const Grass = require("./grass")
+const GrassEater = require("./grassEater")
+const Predator = require("./predator")
+const Agrav = require("./agrav")
+const Mexu = require("./mexu")
+const Mrjyun = require("./mrjyun")
+
+function creatureObj() {
+    for (let y = 0; y < matrix.length; y++) {
+        for (let x = 0; x < matrix[y].length; x++) {
+
+            if (matrix[y][x] == 1) {
+                var gr = new Grass(x, y)
+                grassArr.push(gr)
+            } else if (matrix[y][x] == 2) {
+                var grEat = new GrassEater(x, y)
+                grassEaterArr.push(grEat)
+            } else if (matrix[y][x] == 3) {
+                var pred = new Predator(x, y)
+                predatorArr.push(pred)
+            } else if (matrix[y][x] == 4) {
+                var mrj = new Mrjyun(x, y)
+                mrjyunArr.push(mrj)
+            } else if (matrix[y][x] == 5) {
+                var ag = new Agrav(x, y)
+                agravArr.push(ag)
+            } else if (matrix[y][x] == 6) {
+                var mex = new Mexu(x, y)
+                mexuArr.push(mex)
+            }
+
+        }
+
+    }
+}
+
+
+creatureObj()
+
+function gameMove() {
+    for (let i in predatorArr) {
+        predatorArr[i].eat()
+    }
+    for (let i in mrjyunArr) {
+        mrjyunArr[i].eat()
+    }
+    for (let i in agravArr) {
+        agravArr[i].eat()
+
+    }
+    for (let i in mexuArr) {
+        mexuArr[i].eat()
+
+    }
+}
+
+setInterval(gameMove,1000)
